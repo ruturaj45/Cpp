3ba33e01429cadbb3883f7439ad9d145eabed3cb
Ruturaj Biswal <106529938+ruturaj45@users.noreply.github.com>
2022-06-17 08:14:58 +0100

    Pattern 1st

---
+++ Rectangle_Star_Pattern.cpp
@@ -0,0 +1,19 @@
+// For Printing rectangle Pattern Using *
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 7; j++)
+        {
+            cout << " *";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
